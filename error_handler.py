85855902e7878918ac381257556c465efceb7e9a
maxCut <maxCut@github.com>
2019-02-11 04:37:28 -0500

    added new methods to error handling to enable more detail about diagnosis

--- error_handler.py
+++ error_handler.py
@@ -5,15 +5,30 @@ def Error_Handler(data):
     bp2_reading = data.get(“blood_pressure2”)
     bo_reading = data.get(“blood_oxygen”)
 
-    alert = false
+    alert = False
 
     if bp1_reading > 140 or bp1_reading < 120 or bp2_reading > 90 or bp2_reading < 80:
-        alert = true
+        alert = True
 
     if hr_reading > 120 or hr_reading < 50:
-        alert = true
+        alert = True
 
     if bo_reading > 110 or bo_reading < 70:
-        alert = true
+        alert = True
 
     return alert
+
+def check_hr(data):
+    hr_reading = data.get("heart_rate")
+    if hr_reading > 120 or hr_reading < 50:
+        return True
+
+def check_bp(data):
+    bp = data.get(“blood_pressure1”)
+    if bp> 140 or bp< 120 
+        return True
+
+def check_bo(data):
+    bo_reading = data.get(“blood_oxygen”)
+    if bo_reading > 110 or bo_reading < 70:
+        return True
